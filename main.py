61feb90f5b5fec04ce3fe8d3764b4b6adce15cc7
alex_resch <alex.resch02.business@gmail.com>
2026-04-03 16:45:00 +0200

    feat: add document processing functionality with support for multiple file types

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from rag-finetuned-8b!")
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, UnstructuredMarkdownLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
-if __name__ == "__main__":
-    main()
+def process_folder(folder_path: str) -> list[Document]:
+    all_chunks = []
+
+    for file in Path(folder_path).iterdir():
+        if file.suffix not in (".pdf", ".txt", ".md"):
+            continue
+        chunks = seperate_in_chunks(str(file))
+
+    return all_chunks
+
+def get_document_loader(path: str):
+    if path is None:
+        raise ValueError("File path is None.")
+    if path.endswith(".pdf"):
+        return PyPDFLoader(path)
+    elif path.endswith(".txt"):
+        return TextLoader(path)
+    elif path.endswith(".md"):
+        return UnstructuredMarkdownLoader(path)
+    raise ValueError(f"Unsupported file type: '{path}'")
+
+
+def seperate_in_chunks(path: str) -> list[Document]:
+    loader = get_document_loader(path)
+    pages = loader.load()
+    chunks = RecursiveCharacterTextSplitter(
+        chunk_size=500, chunk_overlap=50
+    ).split_documents(pages)
+    return chunks
